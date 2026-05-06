e095868baed1b8f164640bee57affe1d585a78bd
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-06 20:27:48 +0200

    feat: added the properties file configuration and the repositories extension

--- src/main/java/com/salesianostriana/dam/ecoshop/repository/CustomerRepository.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/repository/CustomerRepository.java
@@ -1,8 +1,12 @@
 package com.salesianostriana.dam.ecoshop.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.salesianostriana.dam.ecoshop.model.Customer;
+
 @Repository
-public interface CustomerRepository {
+public interface CustomerRepository 
+	extends JpaRepository<Customer, Long>{
 
 }
--- src/main/java/com/salesianostriana/dam/ecoshop/repository/ProductRepository.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.salesianostriana.dam.ecoshop.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.salesianostriana.dam.ecoshop.model.Product;
+
 @Repository
-public interface ProductRepository {
+public interface ProductRepository
+	extends JpaRepository<Product, Long>{
 
 }
