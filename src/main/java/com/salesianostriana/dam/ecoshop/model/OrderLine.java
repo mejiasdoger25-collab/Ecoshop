e1dbdb45cf1fa7c813151d0233ec22102bb0d739
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-07 09:56:28 +0200

    fix: fixed foreign keys anotatinos

--- src/main/java/com/salesianostriana/dam/ecoshop/model/OrderLine.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/model/OrderLine.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ForeignKey;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,12 +28,12 @@ public class OrderLine {
 	
 	//relación con Order
 	@ManyToOne
-	@JoinColumn(name="order_id")//el name en la bbdd es formato camel, no con mayus
+	@JoinColumn(foreignKey = @ForeignKey (name="order_id"))//el name en la bbdd es formato camel, no con mayus
 	private Order order;
 	
 	//relación con Product
 	@ManyToOne
-	@JoinColumn(name="product_id")//el name en la bbdd es formato camel, no con mayus
+	@JoinColumn(foreignKey = @ForeignKey (name="product_id"))//el name en la bbdd es formato camel, no con mayus
 	private Product product;
 
 	
