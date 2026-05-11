de5f5ac1f24454ddc9210f2e5f30dc530bcb4689
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-11 13:17:47 +0200

    added endpoints to product & customer entities

--- src/main/java/com/salesianostriana/dam/ecoshop/service/ProductService.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.dam.ecoshop.service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,10 +16,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductService {
 
+	private List<Product> productList = new ArrayList<Product>();
+	
 	public List<Product> getLista() {
 		return Arrays.asList(
 				new Product(1L, "Naranja", 3.99, 39, 10, "España", true, "Una fruta muy sabrosa", LocalDate.now(), null),//poner clave externa
 				new Product(2L, "Lata reciclada", 2.99, 5, 1, "Francia", true, "Una lata hecha a base de reciclaje", null, null)
 				);		
 	}
+	
+	public void add (Product product) {
+		productList.add(product);
+	}
+	
 }
