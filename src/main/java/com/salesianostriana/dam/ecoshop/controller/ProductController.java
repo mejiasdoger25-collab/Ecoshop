c15a84e1c8e2386cda06f77d3a4611ab4e471ae0
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-14 17:35:31 +0200

    fix: action url corrected

--- src/main/java/com/salesianostriana/dam/ecoshop/controller/ProductController.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/controller/ProductController.java
@@ -44,25 +44,27 @@ public class ProductController {
 	}
 	*/
 	
+	@GetMapping({"/", "/list"})
+	public String findAll (Model model) {
+		model.addAttribute("products", service.findAll());
+		return "products/list";
+	}
+	
 	@GetMapping("/new")
 	public String createForm(Model model) {
 		model.addAttribute("product", new Product());
 		return "products/form";
 	}
 	
-	@PostMapping("/save")
+	@PostMapping("/new/submit")
 	public String save (@ModelAttribute("product") Product product) {
 		service.save(product);
-		return "redirect:/products/";
+		return "redirect:/products/list";
 	}
 	
 	
 	
-	@GetMapping({"/", "/list"})//no tiene url para que se ejecute siempre que se entre en la ruta del requestmapping y así se actualice siempre
-	public String findAll (Model model) {
-		model.addAttribute("products", service.findAll());
-		return "products/list";
-	}
+	
 	
 	
 	
