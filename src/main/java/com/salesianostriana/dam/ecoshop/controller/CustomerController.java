c9425436ae2dfd7e01517674e227f2f3d7630741
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-14 20:42:06 +0200

    feat: added customers view/lsit feature

--- src/main/java/com/salesianostriana/dam/ecoshop/controller/CustomerController.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/controller/CustomerController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.salesianostriana.dam.ecoshop.model.Customer;
 import com.salesianostriana.dam.ecoshop.service.CustomerService;
 
 import lombok.Data;
@@ -14,10 +16,12 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @Data //@Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
 @RequiredArgsConstructor
+@RequestMapping("/customers")
 public class CustomerController {
 
 	private final CustomerService service;
 	
+	/*
 	@GetMapping({"/login"})
     public String login() {
         return "login5"; 
@@ -27,6 +31,13 @@ public class CustomerController {
 	public String processForm (@ModelAttribute ("customer") Model model) {
 		
 		return "redirect:/home";
+	}
+	*/
+	
+	@GetMapping({"/", "/list"})
+	public String findAll (Model model) {
+		model.addAttribute("customer", service.findAll());
+		return "customers/list";
 	}
 	
 }
