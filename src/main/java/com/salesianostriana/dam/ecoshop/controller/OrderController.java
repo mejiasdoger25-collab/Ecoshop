3e626d9433c3c5570eacb4c46947cda53de8f85e
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-14 21:45:13 +0200

    feat: added orders list view feature

--- src/main/java/com/salesianostriana/dam/ecoshop/controller/OrderController.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/controller/OrderController.java
@@ -1,7 +1,11 @@
 package com.salesianostriana.dam.ecoshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.salesianostriana.dam.ecoshop.service.CustomerService;
 import com.salesianostriana.dam.ecoshop.service.OrderService;
 
 import lombok.Data;
@@ -10,7 +14,19 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @Data //@Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
 @RequiredArgsConstructor
+@RequestMapping("/orders")
 public class OrderController {
 
 	private final OrderService service;
+	private final CustomerService customerService;
+	
+	@GetMapping({"/", "/list"})
+	public String findAll (Model model) {
+		model.addAttribute("orders", service.findAll());
+		return "orders/list";
+	}
+	
+	
+	
+	
 }
