ebc4d0740c3d5a5408143ea1e3e47f040f8b3d6e
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-17 22:14:43 +0200

    fix: success url has been set default after a success login form

--- src/main/java/com/salesianostriana/dam/ecoshop/security/config/SecurityConfig.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/security/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
             .formLogin(form -> form
 
                 .loginPage("/login")
+                .defaultSuccessUrl("/", true)
                 .permitAll()
 
             )
