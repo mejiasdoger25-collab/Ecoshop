35f7d321ba12cc8b9835e41e3d729ae78f13fc3b
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-20 23:03:42 +0200

    fix: the name of the error message variable has been unified

--- src/main/java/com/salesianostriana/dam/ecoshop/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/exception/ExceptionControllerAdvice.java
@@ -12,7 +12,7 @@ public class ExceptionControllerAdvice {
 	//Own exception
     @ExceptionHandler(InsufficientStockException.class)
     public String handleInsufficientStock(InsufficientStockException ex, Model model) {
-        model.addAttribute("tittleError", "Insufficient stock");
+        model.addAttribute("titleError", "Insufficient stock");
         model.addAttribute("messageError", ex.getMessage());
         return "error/error"; 
     }
@@ -20,7 +20,7 @@ public class ExceptionControllerAdvice {
     //Own exception 
     @ExceptionHandler(InvalidCertificateException.class)
     public String handleInvalidCertificate(InvalidCertificateException ex, Model model) {
-        model.addAttribute("errorTitulo", "Invalid certificate");
+        model.addAttribute("titleError", "Invalid certificate");
         model.addAttribute("messageError", ex.getMessage());
         return "error/error"; 
     }
@@ -28,7 +28,7 @@ public class ExceptionControllerAdvice {
   //Own exception 
     @ExceptionHandler(InsufficientBalanceException.class)
     public String handleInsufficientBalance(InsufficientBalanceException ex, Model model) {
-        model.addAttribute("tittleError", "Insufficient balance");
+        model.addAttribute("titleError", "Insufficient balance");
         model.addAttribute("messageError", ex.getMessage());
         return "error/error"; 
     }
@@ -38,7 +38,7 @@ public class ExceptionControllerAdvice {
     //Find an Id that doesnt exists
     @ExceptionHandler(NoSuchElementException.class)
     public String handleNotFound(NoSuchElementException ex, Model model) {
-        model.addAttribute("tittleError", "Element not found");
+        model.addAttribute("titleError", "Element not found");
         model.addAttribute("messageError", "The requested product does not exist in our database.");
         return "error/error";
     }
@@ -47,7 +47,7 @@ public class ExceptionControllerAdvice {
     //Illegal or invalid argument in business logic
     @ExceptionHandler(IllegalArgumentException.class)
     public String handleIllegalArgument(IllegalArgumentException ex, Model model) {
-        model.addAttribute("tittleError", "Invalid argument");
+        model.addAttribute("titleError", "Invalid argument");
         model.addAttribute("messageError", ex.getMessage());
         return "error/error";
     }
