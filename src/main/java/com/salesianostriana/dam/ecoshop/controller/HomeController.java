498c96868cf4a9117bffdb4395728e3880f658ff
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-25 20:04:39 +0200

    fix: home temporarily suspended

--- src/main/java/com/salesianostriana/dam/ecoshop/controller/HomeController.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.salesianostriana.dam.ecoshop.controller;
+/*package com.salesianostriana.dam.ecoshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,3 +17,8 @@ public class HomeController {
 	}
 	
 }
+
+
+de momento inútil dado que no aporta nada diff de /products/list, por ende, se podría implementar como mejora de tal forma que /home o / tenga algo distinto 
+a lo que se aprecia en /products/list, como por ejemplo los más vendidos, últimos añadidos, ofertas, poco stock, etc ---->> lógica de negocio
+*/
