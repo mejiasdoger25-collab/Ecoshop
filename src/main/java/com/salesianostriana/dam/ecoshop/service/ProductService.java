67ebc47fed0b8b982999bc1006c4dd7ba94fc459
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-25 14:27:41 +0200

    feat: added product samples

--- src/main/java/com/salesianostriana/dam/ecoshop/service/ProductService.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.ecoshop.model.OrderLine;
@@ -14,6 +15,7 @@ import com.salesianostriana.dam.ecoshop.repository.OrderLineRepository;
 import com.salesianostriana.dam.ecoshop.repository.ProductRepository;
 import com.salesianostriana.dam.ecoshop.service.base.BaseServiceImp;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -23,14 +25,19 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class ProductService extends BaseServiceImp <Product, Long, ProductRepository>{
 
-	public ProductService(ProductRepository repository) {
-        super(repository);
+	
+	private final ProductRepository productRepository;
+	private final List<Product> productList = new ArrayList<Product>();
+
+	
+	public ProductService(ProductRepository productRepository) {
+        super(productRepository);
+        this.productRepository = productRepository;
     }
 	
-	/*
-	private final ProductRepository repository;
-	private List<Product> productList = new ArrayList<Product>();
 	
+	
+	/*
 	public List<Product> getLista() {
 		return Arrays.asList(
 				new Product(1L, "Naranja", 3.99, 39, 10, "España", true, "Una fruta muy sabrosa", LocalDate.now(), null),//poner clave externa
@@ -43,4 +50,58 @@ public class ProductService extends BaseServiceImp <Product, Long, ProductReposi
 	}
 	
 	*/
+	
+	@PostConstruct
+	public void initUsers() {
+
+	    if (productRepository.count() == 0) {
+
+	    	productRepository.save(
+	                Product.builder()
+	                       	.name("Naranja")
+	                       	.price(2)
+	                       	.stock(10)
+	                       	.minimumStock(5)
+	                       	.origin("Fuengirola")
+	                       	.ecoCertificate(true)
+	                       	.description("Una misteriosa fruta de dudosa procedencia")
+	                       	.expirationDate(LocalDate.now().plusWeeks(1))
+	                       	//.lines(null) builder.default
+	                        .build()
+	        );
+
+	    	productRepository.save(
+	    			Product.builder()
+		    				//.id(002L) auto
+		                   	.name("Botella")
+		                   	.price(1.20)
+		                   	.stock(5000)
+		                   	.minimumStock(100)
+		                   	.origin("Madird")
+		                   	.ecoCertificate(true)
+		                   	.description("Realizada con material reciclado")
+		                   	.expirationDate(null)//nunca
+		                   	//.lines(null) builder.default
+		                    .build()
+	    	);
+
+	    	productRepository.save(
+	    			Product.builder()
+		    				//.id(003L) auto
+		                   	.name("Teléfono")
+		                   	.price(500)
+		                   	.stock(10)
+		                   	.minimumStock(2)
+		                   	.origin("China")
+		                   	.ecoCertificate(false)
+		                   	.description("Un dispositivo con tecnología punta")
+		                   	.expirationDate(null)//nunca
+		                   	//.lines(null) builder.default
+		                    .build()
+	    	);
+
+	    }
+
+	}
+	
 }
