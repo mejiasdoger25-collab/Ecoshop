290e21decafa08405ddad8a239af5b23b6f5c24f
Germán Mejías Domínguez <mejias.doger25@triana.salesianos.edu>
2026-05-30 12:08:01 +0200

    fix: pagination buttons, sidebar style & categories are now /public in request matchers

--- src/main/java/com/salesianostriana/dam/ecoshop/security/config/SecurityConfig.java
+++ src/main/java/com/salesianostriana/dam/ecoshop/security/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                     "/css/**",
                     "/javascript/**",
                     "/images/**",//acceso cambiado para el login background
+                    "/category/{id}",
                     "/register"
                 ).permitAll()
 
